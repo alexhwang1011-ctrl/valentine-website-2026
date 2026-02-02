d9c66f7bf7f8561db04b9cd1169353c412644d41
alexhwang1011-ctrl <alexhwang1011@gmail.com>
2026-02-02 15:22:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dli3ypvei/video/upload/v1770063636/Martin_Garrix_Third_Party_-_Carry_You_feat._Oaks_Declan_J_Donovan_Official_Video_wqhklv.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
